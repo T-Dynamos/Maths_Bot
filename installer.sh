951edf64a37a3ffbad067a2f1313a4ee8a644759
T-Dynamos <68729523+T-Dynamos@users.noreply.github.com>
2021-05-02 16:54:34 +0530

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,14 @@
+figlet   INSTALLING... | lolcat
+cyan='\e[0;36m'
+lightgreen='\e[1;32m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+echo -e $lightgreen "\e[32m Maths bot Setup Wizard"
+echo -e $lightgreen "\e[32m Wait Patiently..."
+pkg upgrade
+pip install random
+pip install colorama
+pip install maths
+echo -e  ==========================
+echo -e     Run python setup.py 
+echo -e  ==========================
