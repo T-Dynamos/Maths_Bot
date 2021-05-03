0f6dfc61ad13464aa7ef17a6feebc78c8faf6c21
T-Dynamos <68729523+T-Dynamos@users.noreply.github.com>
2021-05-03 13:37:42 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,6 @@
 clear
 pkg install figlet
-pkg install lolcat
-figlet   INSTALLING... | lolcat
+figlet   INSTALLING... 
 cyan='\e[0;36m'
 lightgreen='\e[1;32m'
 red='\e[1;31m'
@@ -9,7 +8,7 @@ yellow='\e[1;33m'
 echo -e $lightgreen "\e[32m Maths bot Setup Wizard"
 echo -e $lightgreen "\e[32m Wait Patiently..."
 pkg upgrade
-pip install random
+pip install requests
 pip install colorama
 clear
 python setup.py
