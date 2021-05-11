09f3c788207aaffb15e61e0cb085e65277aac8d1
T-Dynamos <68729523+T-Dynamos@users.noreply.github.com>
2021-05-11 12:02:54 +0530

    Add files via upload

---
+++ wininstall.bat
@@ -0,0 +1,8 @@
+clear
+echo -e INSTALLING
+python -m pip install requests
+python -m pip install table-ex
+python -m pip install colorama
+sudo dnf install git-all
+git clone https://github.com/T-Dynamos/Maths_Bot
+python setup.py
