a40160891c66d4569164b4033d2b0ae1fffabbff
T-Dynamos <68729523+T-Dynamos@users.noreply.github.com>
2021-06-02 17:23:40 +0530

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,17 +1,10 @@
 clear
-apt update
-apt upgrade 
-apt install figlet -y
-apt install toilet -y
-apt install nano -y
-apt install ruby -y
-apt install curl -y
-gem install lolcat
+apt update 
+apt upgrade -y
 clear
-toilet -f pagga "Installing" | lolcat
+echo -e Installing ...
 pip install colorama
 pip install requests
 pip install table-ex
 cd m_bot
-toilet -f pagga "Success..!!" | lolcat
-python setup.py
+python3 setup.py
