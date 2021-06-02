52fbc1a83b88c670bed1c2d33e082a8cdf9d803b
T-Dynamos <68729523+T-Dynamos@users.noreply.github.com>
2021-06-02 18:13:32 +0530

    Update wininstall.bat

--- wininstall.bat
+++ wininstall.bat
@@ -3,6 +3,6 @@ echo -e INSTALLING
 python -m pip install requests
 python -m pip install table-ex
 python -m pip install colorama
-sudo dnf install git-all
+winget install git
 git clone https://github.com/T-Dynamos/Maths_Bot
 python setup.py
