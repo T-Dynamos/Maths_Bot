3fd7c7f740b2af4edef249a6ebf0ac3b17f0b850
T-Dynamos <68729523+T-Dynamos@users.noreply.github.com>
2021-06-05 09:44:09 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,10 +1,12 @@
 clear
 apt update 
 apt upgrade -y
+apt install wget
+cd /data/data/com.termux/files/usr/bin
+wget https://raw.githubusercontent.com/T-Dynamos/termux-pro/main/mbot
+cd
 clear
 echo -e Installing ...
 pip install colorama
 pip install requests
 pip install table-ex
-cd m_bot
-python3 setup.py
